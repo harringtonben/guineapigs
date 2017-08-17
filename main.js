b266198b78f2b7621b4d55a879818decb8ac2173
ben harrington <harringtonben@icloud.com>
2017-08-17 17:34:49 -0500

    finished dom output function

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 console.log("I'm in main JS");
 
+var outputTarget = document.getElementById('output-target');
 // H1 hover function
 document.body.addEventListener('mouseover', function(event) {
 		if (event.target.id === 'page-title') {
-			console.log('you are in the header');}
+			// console.log('you are in the header');
+			printOutput('you are in the header');}
 });
 
 document.body.addEventListener('mouseout', function(event) {
 		if (event.target.id === 'page-title') {
-			console.log('you moved out of the header');}
+			// console.log('you moved out of the header');
+			printOutput('you moved out of the header');}
 });
 
 //Section click function
 
 document.body.addEventListener('click', function(event){
 	if (event.target.className === 'article-section') {
-		console.log(event.target.innerHTML);}
+		// console.log(event.target.innerHTML);
+		printOutput(event.target.innerHTML);}
 });
 
-//next function will go here
+//Print To Dom Function
+
+function printOutput(input) {
+	// console.log('printOutput:',input);
+	var printString = '';
+	printString += `<div>`;
+	printString +=    `<h3>${input}</h3>`;
+	printString += `</div>`;
+	outputTarget.innerHTML = printString;	
+}
 
