51136635c29405a3f9ec68e7d361e1d7db10ae3a
ben harrington <harringtonben@icloud.com>
2017-08-17 11:45:52 -0500

    added the header hover functions

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("I'm in main JS");
+
+// H1 hover function
+document.body.addEventListener('mouseover', function(event) {
+		if (event.target.id === 'page-title') {
+			console.log('you are in the header');}
+});
+
+document.body.addEventListener('mouseout', function(event) {
+		if (event.target.id === 'page-title') {
+			console.log('you moved out of the header');}
+});
+
